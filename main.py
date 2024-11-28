f45b0ac95c6bfaafc69ba2540d98e59d70c01f27
Marco Castañeda <marco.castaneda@kiranalabs.mx>
2024-11-28 11:50:40 -0600

    Coppel scrapper

--- main.py
+++ main.py
@@ -323,7 +323,32 @@ def check_home_depot(url):
                 (review if review is not None else "-"))
     except requests.RequestException:
         return "Failed to fetch the page"
+    
+def check_coppel(url):
+    #url must be https://www.coppel.com.mx/$product
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            discounted_price = None
+            original_price = None
+            soup = BeautifulSoup(response.text, 'html.parser')
 
+            discounted_price = soup.find('h2', {'data-testid': 'pdp_discounted_price'})
+
+            if discounted_price is not None:
+                original_price = soup.find('p', {'data-testid': 'pdp_price'})
+            else:
+                original_price = soup.find('h2', {'data-testid': 'pdp_price'})
+
+
+            if discounted_price is None and original_price is None:
+                return "PAGINA NO ENCONTRADA", "-", "-", "-", "-"
+                
+            return "ACTIVO", (original_price.text if original_price is not None else "-"), (discounted_price.text if discounted_price is not None else "-"),  "-", "-"
+        else:
+            return "INACTIVO", "-", "-", "-", "-"
+    except requests.RequestException as e:
+            return "PAGINA NO ENCONTRADA", "-", "-", "-", "-"
 
 def main():
     # mas de un archivo
@@ -387,6 +412,9 @@ def main():
                 result, price, promotion_price, rating, reviews = check_walmart(link)
             elif marketplace == 'HomeDepot':
                 result, price, promotion_price, rating, reviews = check_home_depot(
+                    link)
+            elif marketplace == 'Coppel':
+                result, price, promotion_price, rating, reviews = check_coppel(
                     link)
 
             row = [
