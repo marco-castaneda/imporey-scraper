1f724ac0b94efb431b477340f7d3c8fa885e3245
Marco Castañeda <marco.castaneda@kiranalabs.mx>
2024-12-17 13:56:12 -0600

    home depot

--- main.py
+++ main.py
@@ -20,7 +20,8 @@ from scrappers import (
     check_amazon, 
     check_liverpool,
     check_mercadolibre, 
-    check_walmart
+    check_walmart,
+    check_home_depot
 )
 
 
@@ -31,43 +32,7 @@ def chunk_list(lst, chunk_size):
 
 
 
-def check_home_depot(url):
-    try:
 
-        last_part = url.split("/")[-1]
-        last_value = last_part.split("-")[-1]
-        home_depot_request = f'https://www.homedepot.com.mx/search/resources/api/v2/products?langId=-5&storeId=10351&contractId=4000000000000000003&langId=-5&partNumber={last_value}&physicalStoreId=8702'
-        response = requests.get(home_depot_request)
-        data = response.json()
-        promotion_price = None
-        rating = None
-        review = None
-        list_price = None
-        status = "INACTIVO"
-        base_data = data["contents"][0]
-        for price_pos in base_data["price"]:
-            if price_pos["usage"] == "Offer":
-                promotion_price = price_pos["value"]
-            if price_pos["usage"] == "Display":
-                list_price = price_pos["value"]
-        if "x_ratings.total_reviews" in base_data:
-            review = base_data["x_ratings.total_reviews"]
-        if "x_ratings.rating" in base_data:
-            rating = base_data["x_ratings.rating"]
-        product_id = base_data["id"]
-        status_request = f'https://www.homedepot.com.mx/wcs/resources/store/10351/inventoryavailability/{product_id}?&langId=-5&onlineStoreId=10351&search=2&physicalStoreId=12605'
-        response = requests.get(status_request)
-        availabilty_data = response.json()
-        if availabilty_data["InventoryAvailability"][0][
-                "inventoryStatus"] == "Available":
-            status = "ACTIVO"
-
-        return (status, (list_price if list_price is not None else "-"),
-                (promotion_price if promotion_price is not None else "-"),
-                (rating if rating is not None else "-"),
-                (review if review is not None else "-"))
-    except requests.RequestException:
-        return "Failed to fetch the page"
 
 def check_coppel(url):
     #url must be https://www.coppel.com.mx/$product
--- scrappers/__init__.py
+++ scrappers/__init__.py
@@ -2,3 +2,4 @@ from .amazon import *
 from .liverpool import *
 from .mercado_libre import *
 from .walmart import *
+from .home_depot import *
