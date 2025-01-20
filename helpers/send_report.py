18f38382a4de0af263e46ed5524afeeef543691a
Marco Castañeda <marco.castaneda@kiranalabs.mx>
2025-01-20 10:42:54 -0600

    test email railway

--- helpers/send_report.py
+++ helpers/send_report.py
@@ -3,7 +3,9 @@ from sendgrid import SendGridAPIClient # type: ignore
 
 
 def make_report():
+    try:
         sendgrid_client = SendGridAPIClient(api_key=os.getenv("SENDGRID_API_KEY"))
+        
         data = {
             "from": {"email": "marco.castaneda@kiranalabs.mx"},
             "personalizations": [
@@ -12,17 +14,23 @@ def make_report():
                         {"email": "marco_pascual410@hotmail.com"},
                     ],
                     "dynamic_template_data": {
-                        "title": "Se creo una nueva solicitud de facturación marketplace por parte de cliente.",
+                        "title": "Se creó una nueva solicitud de facturación marketplace por parte de cliente.",
                     },
                 }
             ],
             "template_id": "d-c17ab97bebf04d08a6d0a5ff2f30e98b",
         }
-
+        
         response = sendgrid_client.client.mail.send.post(request_body=data)
-
-        print("Report finished 2")
-        print("Report finished")
-        print("response")
-        print(response)
+        
+        if 200 <= response.status_code < 300:
+            print("Correo enviado exitosamente.")
+            print(f"Estado: {response.status_code}, Detalles: {response.body.decode()}")
+        else:
+            print("Error al enviar el correo.")
+            print(f"Estado: {response.status_code}, Detalles: {response.body.decode()}")
+    
+    except Exception as e:
+        print("Ocurrió un error al intentar enviar el correo:")
+        print(str(e))
 
